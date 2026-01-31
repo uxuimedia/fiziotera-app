6fa96aa53a28c65a1af4ec984fb36f70267cc98e
Sergei Ermilov <sergei.m.ermilov@gmail.com>
2026-01-31 11:47:51 +0300

    new old landing added

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+/*
+All JS goes here
+*/
+
+var mymap = L.map('mapid').setView([42.2924, 18.8430], 14);
+
+//42.292417896436945, 18.84300201680142
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
+	mymap.scrollWheelZoom.disable();
+
+	L.marker([42.2924, 18.8430]).addTo(mymap)
+		.bindPopup("<b>Fiziotera</b>").openPopup();
+
+
+	var popup = L.popup();
+
+	$(document).ready(function(){
+		$(window).scroll(function () {
+				if ($(this).scrollTop() > 50) {
+					$('#back-to-top').fadeIn();
+				} else {
+					$('#back-to-top').fadeOut();
+				}
+			});
+			// scroll body to 0px on click
+			$('#back-to-top').click(function () {
+				$('body,html').animate({
+					scrollTop: 0
+				}, 400);
+				return false;
+			});
+	});
